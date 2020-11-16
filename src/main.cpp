464a94abf0c90c14dd614223824123785d8f6455
Tomislav Prusina <tprusina@mathos.hr>
2020-11-16 14:59:51 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main(int args, char **cargs)
 {
 
+    // ??? Don't know why I put this here :(
     if (args < 2)
     {
         cerr << "program takes file name";
         return EXIT_FAILURE;
     }
     
-    vector<token> tokens = tokenize_file(cargs[1]);
+    vector<token> tokens = tokenize_file(cargs[2]);
     for(auto [i, j, col, row] : tokens)
         (i ? cout : cerr) << cargs[1] << ':' << row
                           << ':' << col << ": " << i
