c9fdc728ba12d224368adc77cc202631adc5b75c
Tomo <tprusina@mathos.hr>
2021-03-01 17:35:43 +0100

    Upgraded tester.py

--- tests/tester.py
+++ tests/tester.py
@@ -1,24 +1,28 @@
 import subprocess as subpr
 import os
+from pathlib import Path
 
-for i in os.listdir('./'):
+testpath = Path(__file__).parent
+c4path = testpath.parent
+
+for i in os.listdir(testpath):
     if '.c' in i:
         res = subpr.run([
-            '../build/debug/c4',
+            c4path / './build/debug/c4',
             '--print-ast',
-            i
+            testpath / i
         ], capture_output = True)
 
         ccres = subpr.run([
             'cc',
             '-fsyntax-only',
             '--pedantic',
-            i
+            testpath / i
         ], capture_output = True)
 
         if res.returncode:
             if not ccres.returncode:
-                print(i, 'unexpected pass')
+                print(i, 'unexpected FAIL')
         else:
             if ccres.returncode:
-                print(i, 'unexpected fail')
+                print(i, 'unexpected PASS')
