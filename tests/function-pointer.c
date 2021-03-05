f0a28f0d797197d21b3e129fc8c5028f02296e3b
Tomo <tprusina@mathos.hr>
2021-03-05 23:30:39 +0100

    function-pointer.c

--- tests/function-pointer.c
+++ tests/function-pointer.c
@@ -1,1 +1,18 @@
 void** (*f)(void**);
+
+void g(void (*c)(void)) {
+
+    c();
+    return ;
+}
+
+void d(void) {
+
+    return ;
+}
+
+int main(void) {
+    g(d);
+
+    return 0;
+}
---
+++ tests/ref.c
@@ -0,0 +1,13 @@
+int printf(const char*, ...);
+
+void f(void) {
+
+    return ;
+}
+
+int main(void) {
+
+    printf("%d\n", &f);
+
+    return 0;
+}
